8ad0f21d6f49d482eaea25f5ff03eef6ec1f6149
Om Panigrahi <ompani6167@gmail.com>
2025-02-07 18:02:10 +0530

    07-02-2025

--- src/container/About.jsx
+++ src/container/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import Foter from "../components/footer";
+import Footer from "../components/Footer";
 
 const About = () => {
   return (
@@ -13,13 +13,13 @@ const About = () => {
         <div className="about-text">
           <h3>Hello Clients,</h3>
           <p>
-            At <strong className="text-green-800 underline" alt="">Balaji Services</strong>, we are committed to providing top-quality home and garden services to keep your surroundings clean, beautiful, and well-maintained. Our expert team specializes in tree cutting for safety and aesthetics, house coloring to refresh your home’s look, and housekeeping for a spotless living space. We also offer plot cleaning to clear debris, garden maintenance to nurture your greenery, and grass cutting for a well-manicured lawn. With a focus on professionalism, efficiency, and customer satisfaction, we ensure every service meets the highest standards, helping you create a clean, organized, and visually appealing environment.
+            At <strong className="text-green-800 underline">Balaji Services</strong>, we are committed to providing top-quality home and garden services to keep your surroundings clean, beautiful, and well-maintained. Our expert team specializes in tree cutting for safety and aesthetics, house coloring to refresh your home’s look, and housekeeping for a spotless living space. We also offer plot cleaning to clear debris, garden maintenance to nurture your greenery, and grass cutting for a well-manicured lawn. With a focus on professionalism, efficiency, and customer satisfaction, we ensure every service meets the highest standards, helping you create a clean, organized, and visually appealing environment.
           </p>
         </div>
       </div>
       {/* Footer */}
       <footer className="about-footer">
-        <Foter />
+        <Footer />
       </footer>
     </div>
   );
--- src/container/Navbar.jsx
+++ src/container/Navbar.jsx
@@ -8,13 +8,32 @@ function Navbar() {
     <>
       <nav className="navbar">
         <div className="nav-left w-32 text-white">
-<Link to="/"><img src="/images/logo.png" alt="" /></Link>
+          <Link to="/">
+            <img src="/images/logo.png" alt="" />
+          </Link>
         </div>
         <div className="nav-right">
-          <Link className="hover:text-[#2fe29e] transition duration-150" to="/">Home</Link>
-          <Link className="hover:text-[#2fe29e] transition duration-150"  to="/service">Service</Link>
-          <Link className="hover:text-[#2fe29e] transition duration-150" to="/about">About</Link>
-          <Link className="hover:text-[#2fe29e] transition duration-150" to="/contact">Contact</Link>
+          <Link className="hover:text-[#2fe29e] transition duration-150" to="/">
+            Home
+          </Link>
+          <Link
+            className="hover:text-[#2fe29e] transition duration-150"
+            to="/service"
+          >
+            Service
+          </Link>
+          <Link
+            className="hover:text-[#2fe29e] transition duration-150"
+            to="/about"
+          >
+            About
+          </Link>
+          <Link
+            className="hover:text-[#2fe29e] transition duration-150"
+            to="/contact"
+          >
+            Contact
+          </Link>
         </div>
       </nav>
     </>
