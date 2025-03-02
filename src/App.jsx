904ee69849138fb1d3b1659bc9a1895479f79e63
Om Panigrahi <ompani6167@gmail.com>
2025-03-02 23:39:22 +0530

    Redirects to index only

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { BrowserRouter as Router, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+
 import Navbar from "./container/Navbar";
+// import Contactus from "./container/Contact";
 import "./App.css";
 import Routes from "./Routes";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <SaveLastVisitedRoute />
-      <Navbar />
-      <Routes />
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Routes />
+      </Router>
+    </>
   );
-}
-
-// Function to save the last visited route in localStorage
-function SaveLastVisitedRoute() {
-  const location = useLocation();
-
-  useEffect(() => {
-    localStorage.setItem("lastVisited", location.pathname);
-  }, [location]);
-
-  return null;
-}
-
-// Redirect to last visited page on initial load
-const lastVisited = localStorage.getItem("lastVisited");
-if (lastVisited && lastVisited !== window.location.pathname) {
-  window.location.pathname = lastVisited;
 }
 
 export default App;
